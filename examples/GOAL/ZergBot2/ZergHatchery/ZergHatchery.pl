d1a06a248201d654f76369b5fb8cbea6417bc07f
Dahny <P.C.plenge@student.tudelft.nl>
2016-04-28 21:38:37 +0200

    Changed ConstructionSite and recreated the ZergBot. The Zerg bot starts lagging very hard when the agent count is above 10. Also changed friendly/9 to friendly/7 and enemy/9 tot enemy/7. Also changed a couple of things in the Bridge and removed some unnecessary Percepts/Perceivers.

---
+++ examples/GOAL/ZergBot2/ZergHatchery/ZergHatchery.pl
@@ -0,0 +1,16 @@
+:- dynamic
+done/0,
+drone/2,
+id/1,
+queueSize/1,
+friendly/5,
+isMorphing/2,
+builder/0,
+minerals/1,
+unit/2,
+gas/1,
+supply/2,
+trainOverlord/0,
+trainDrone/0,
+trainHydralisk/0,
+trainZergling/0.
