b58c05c54f5e19728a2a5d59c30e256d79e09bcb
Dahny <P.C.plenge@student.tudelft.nl>
2016-05-04 13:43:42 +0200

    Start of the protoss bot. The probes mine and the nexus makes some probes a.t.m. Still need to fix ConstructionSite

---
+++ examples/GOAL/ProtossBot2/ProtossCybernaticsCore/ProtossCybernaticsCore.pl
@@ -0,0 +1,5 @@
+:- dynamic
+supply/2,
+gas/1,
+minerals/1,
+upgrade/1.  
---
+++ examples/GOAL/ProtossBot2/ProtossProbe/ProtossProbe.pl
@@ -0,0 +1,43 @@
+ :- dynamic
+builder/0,
+minerals/1,
+gathering/1,
+unit/2,
+gas/1,
+mineralField/4,
+supply/2,
+position/2,
+constructionSite/2,
+workerActivity/2,
+id/1,
+me/1,
+chokepoint/2,
+base/4,
+constructing/0,
+friendly/5,
+vespeneGeyser/3,
+gather/1,
+build/3.
+
+
+		busy :- constructing ; gathering(_).
+		cost("Protoss Nexus", 400, 0).
+		cost("Protoss Gateway", 150, 0).
+		cost("Protoss Assimilator", 100, 0).
+		cost("Protoss Cybernetics Core", 200, 0).
+						
+		distance(X1,Y1,X2,Y2,D) :- D is sqrt((X2-X1)**2 + (Y2-Y1)**2).
+		
+		chokepointlocation(X,Y, RX,RY) 
+			:-	findall([D,BX,BY], (chokepoint(BX,BY), distance(X,Y,BX,BY,D)), L),
+				sort(L, [[_,RX,RY]|_]).
+				
+	
+		shouldMineVespene :- unit("Protoss Assimilator",RefCount),
+				NeededWorkers is RefCount * 2,
+				aggregate_all(count, workerActivity(_,gatheringGas), Count),
+				Count < NeededWorkers.
+								
+		nearestBase(X, Y, RX, RY) 
+		:-	findall([D,BX,BY], (base(BX,BY, 'false', _), distance(X,Y,BX,BY,D)), L),
+			sort(L, [[_,RX,RY]|_]). 	
