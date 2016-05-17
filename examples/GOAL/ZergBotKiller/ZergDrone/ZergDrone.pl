f926d9c88da71f3358a3cfe6cce90cb21c2131bb
Dahny <P.C.plenge@student.tudelft.nl>
2016-05-17 12:48:16 +0200

    updated percepts and zerg bot

--- examples/GOAL/ZergBotKiller/ZergDrone/ZergDrone.pl
+++ examples/GOAL/ZergBotKiller/ZergDrone/ZergDrone.pl
@@ -1,19 +1,19 @@
  :- dynamic
+condition/1,
+minerals/1,
+gas/1,
 builder/0,
-resources/4,
 gathering/1,
 unit/2,
 mineralField/4,
 position/2,
 constructionSite/2,
 workerActivity/2,
-isStuck/0,
 self/2,
-me/1,
 chokepoint/2,
 base/4,
 constructing/0,
-friendly/5,
+friendly/6,
 vespeneGeyser/3,
 buildPool/2,
 buildExtractor/2,
@@ -26,25 +26,22 @@ isStuck/0,
 stop/0,
 build/3,
 isMorphing/2,
-move/2.
+move/2,
+unitAmount/2.
 
-		busy :- constructing ; gathering(_).
+		busy :- condition(List), member('gathering', List).
+		
 		cost("Zerg Spawning Pool", 200, 0).
 		cost("Zerg Evolution Chamber", 75, 0).
 		cost("Zerg Extractor", 75, 0).
 		cost("Zerg Hatchery", 300, 0).
 		cost("Zerg Hydralisk Den", 100, 50).
 		cost("Zerg Creep Colony", 75, 0).
-		cost("Zerg Queens Nest", 150, 100).
 						
 		distance(X1,Y1,X2,Y2,D) :- D is sqrt((X2-X1)**2 + (Y2-Y1)**2).
-		
-		chokepointlocation(X,Y, RX,RY) 
-			:-	findall([D,BX,BY], (chokepoint(BX,BY), distance(X,Y,BX,BY,D)), L),
-				sort(L, [[_,RX,RY]|_]).
 				
 	
-		shouldMineVespene :- unit("Zerg Extractor",RefCount),
+		shouldMineVespene :- unitAmount("Zerg Extractor",RefCount),
 				NeededWorkers is RefCount * 2,
 				aggregate_all(count, workerActivity(_,gatheringGas), Count),
 				Count < NeededWorkers.
---
+++ examples/GOAL/ZergBotKiller/ZergHydraliskDen/ZergHydraliskDen.pl
@@ -0,0 +1,6 @@
+:- dynamic
+supply/2,
+gas/1,
+minerals/1,
+upgrading/1,
+hasResearched/1. 
