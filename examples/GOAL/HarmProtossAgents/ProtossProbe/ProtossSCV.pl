ef7381a10e9a86c237348793cc56238470bb6e55
HarmGriffioen <harmgriffioen@gmail.com>
2016-05-18 09:48:53 +0200

    Updated HarmBot

--- examples/GOAL/HarmProtossAgents/ProtossProbe/ProtossSCV.pl
+++ examples/GOAL/HarmProtossAgents/ProtossProbe/ProtossSCV.pl
@@ -7,7 +7,7 @@ gas/1,
 mineralField/4,
 supply/2,
 position/2,
-constructionSite/2,
+constructionSite/3,
 refinery/3,
 workerActivity/2,
 id/1,
@@ -24,36 +24,17 @@ ownbase/2,
 cmd/2,
 pos/2,
 attacking/2,
-unit/3.
+unit/3,
+condition/1,
+unitAmount/2.
 
 		busy :- condition(L),(member('constructing',L) ; member('gathering',L)).
-		cost("Terran Command Center",400,0).
-		cost("Terran Supply Depot", 100, 0).
-		cost("Terran Barracks", 150, 0).
-		cost("Terran Bunker", 100, 0).
-		cost("Terran Refinery", 100, 0).
-		cost('Terran Academy',150,0).
-		cost("Terran Engineering Bay", 125, 0).
-		cost("Terran Factory", 200, 100).
-		cost('Terran Starport',150,100).
-		cost("Terran Science Facility", 100, 150).
-		condition("Terran Academy")
-			:- friendly(_, "Terran Barracks", _, _, _).
-		condition("Terran Supply Depot") 
-			:- supply(C, Max), Max - C < 6.
-		condition("Terran Barracks") 
-			:- supply(C, Max), Max >= 20, Max - C > 6,
-				((unit("Terran Barracks",Num), Num < 3) ; not(unit("Terran Barracks",_))).
-		condition("Terran Refinery").
+		cost("Protoss Pylon", 100, 0).
+		cost("Protoss Gateway", 150, 0).
 								
 		buildingLocation(X,Y, RX,RY) 
-			:-	findall([D,BX,BY], (constructionSite(BX,BY), distance(X,Y,BX,BY,D)), L),
+			:-	findall([D,BX,BY], (constructionSite(BX,BY,'true'), distance(X,Y,BX,BY,D)), L),
 				sort(L, [[_,RX,RY]|_]). 
-				
-		addonBuildingLocation(RX, RY)
-			:-	friendly(_, "Terran Command Center", _, X, Y),
-				findall([D,BX,BY], (constructionSite(BX,BY), distance(X,Y,BX,BY,D), (constructionSite(HX, BY), HX is X + 1)), L),
-				sort(L, [[_,RX,RY]|_]).
 						
 		distance(X1,Y1,X2,Y2,D) :- D is sqrt((X2-X1)**2 + (Y2-Y1)**2).
 		
@@ -73,6 +54,14 @@ unit/3.
 		buildLocation(X, Y) :- 
 				ownbase(TX, TY),
 				buildingLocation(TX, TY, X, Y).
+				
+		buildPylonLocation(X, Y) :- 
+				ownbase(TX, TY),
+				buildingPylonLocation(TX, TY, X, Y).
+						
+		buildingPylonLocation(X,Y, RX,RY) 
+			:-	findall([D,BX,BY], (constructionSite(BX,BY,_), distance(X,Y,BX,BY,D)), L),
+				sort(L, [[_,RX,RY]|_]). 
 		
 		commandCenter(X,Y)
 			:- friendly(_, "Terran Command Center", _ , X, Y).
